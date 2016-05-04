60cbed38d6e8c70b472ed0fdafbb211ea645fb43
zxy <haoqqemail@qq.com>
2016-05-04 18:46:09 +0800

    init

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,95 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+ $ionicConfigProvider.tabs.position('bottom'); // other values: top
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+
+
+  // setup an abstract state for the tabs directive
+  $stateProvider.state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+ .state('tab.massage', {
+      url: '/massage',
+      views: {
+        'tab-massage': {
+          templateUrl: 'templates/tab-massage.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+
+  .state('tab.qushi', {
+    url: '/qushi',
+    views: {
+      'tab-qushi': {
+        templateUrl: 'templates/tab-qushi.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+ 
+  .state('tab.yujing', {
+      url: '/yujing',
+      views: {
+        'tab-yujing': {
+          templateUrl: 'templates/tab-yujing.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/massage');
+
+});
